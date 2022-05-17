b929ab62370d9bf8d7c6b1fb75ff1728f3ddca72
MirMello <mirandaleeallen@gmail.com>
2022-05-16 19:00:33 -0600

    added code and changed questions to what I want

---
+++ dist/generated-README.md
@@ -0,0 +1,55 @@
+
+    # ReadME Generator
+    
+     
+  
+    ## Table-of-Contents
+  
+    * [Description](#description)
+    * [Installation](#installation)
+    * [Usage](#usage)
+     
+    * [Contributing](#contributing)
+    * [Tests](#tests)
+    * [Questions](#questions)
+    
+    ## [Description](#table-of-contents)
+  
+    This can help solve lack of professionalism in README files
+  
+    I built this to save time and make professional looking README files
+  
+    To install this README Generator, type the following into your command line: node install inquirer node index
+  
+    ## [Installation](#table-of-contents)
+  
+    download the code onto your computer, in the command line, enter npm install inquirer to install
+  
+    ## [Usage](#table-of-contents)
+  
+    Type in your command line: node index, to use the code to create your ReadMe File
+    
+    For more information on how to add screenshots for examples, visit the following website:
+    
+    [Mark Down Tutorial](https://agea.github.io/tutorial.md/)
+    
+     
+  
+    ## [Contributing](#table-of-contents)
+    
+    
+    Thank you for your interest in helping out; however, I will not be accepting contributions from third parties.
+      
+  
+    ## [Tests](#table-of-contents)
+  
+    A walkthrough video will be added
+  
+    ## [Questions](#table-of-contents)
+  
+    Please contact me using the following links:
+  
+    [GitHub](https://github.com/MirMello)
+  
+    [Email: mirandaleeallen@gmail.com](mailto:mirandaleeallen@gmail.com)
+  
--- index.js
+++ index.js
@@ -1,13 +1,185 @@
-// TODO: Include packages needed for this application
+// Packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter your title.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub Username? (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'what',
+        message: 'What was your motivation for creating this project? (Required)',
+        validate: whatInput => {
+            if (whatInput) {
+                return true;
+            } else {
+                console.log('Please enter what your modivation was.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'why',
+        message: 'Why did you build this project? (Required)',
+        validate: whyInput => {
+            if (whyInput) {
+                return true;
+            } else {
+                console.log('Please enter why you created this project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'problem',
+        message: 'What problem does it solve? (Required)',
+        validate: howInput => {
+            if (howInput) {
+                return true;
+            } else {
+                console.log('Please enter what problem it solves.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'learn',
+        message: 'What did you learn?(Required)',
+        validate: installInput => {
+            if (installInput) {
+                return true;
+            } else {
+                console.log('Please enter what you learned.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'standOut',
+        message: 'What makes your project stand out? (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('Please enter what makes it stand out.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running. (required)',
+        choices: ['agpl', 'apache', 'mit', 'no license']
+    },
+    {
+        type: 'input',
+        name: 'Usage',
+        message: 'Provide instructions and examples for use.',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'Please provide guidelines for contributing. (Required)',
+        when: ({ confirmContributers }) => {
+            if (confirmContributers) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: contributerInput => {
+            if (contributerInput) {
+                return true;
+            } else {
+                console.log('Please enter contributer guidelines!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide instructions on how to test the app. (Required)',
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('Please enter your use test instructions!');
+                return false;
+            }
+        }
+    }
+];
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// function to write README file
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/generated-README.md', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
 
-// TODO: Create a function to initialize app
-function init() {}
+// function to prompt questions and store user inputs
+const init = () => {
+
+    return inquirer.prompt(questions)
+    .then(readmeData => {
+        return readmeData;
+    })
+}
 
 // Function call to initialize app
-init();
+init()
+.then(readmeData => {
+    console.log(readmeData);
+    return generateMarkdown(readmeData);
+})
+.then(pageMD => {
+    return writeFile(pageMD);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse.message);
+})
+.catch(err => {
+    console.log(err);
+})
